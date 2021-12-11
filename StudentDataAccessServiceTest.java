9d51dfe96567bda30af1c2ca52db976d38ea6cf5
Lennin Davila <lennin.davila@gmail.com>
2021-12-11 04:31:05 -0600

    repo3

--- StudentDataAccessServiceTest.java
+++ StudentDataAccessServiceTest.java
@@ -33,49 +33,49 @@ public class StudentDataAccessServiceTest {
 
         dataSource = new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
-                .addScript("/dbunit/init.sql")
+                .addScript("/repo3/repo3.sql")
                 .build();
 
         jdbcTemplate.setDataSource(dataSource);
 
         studentDataAccessService = new StudentDataAccessService(jdbcTemplate);
     }
-
+//repo3
     @Test
     public void validateASelect() {
         List<Student> students = studentDataAccessService.selectAllStudents();
         assertEquals(students.size(), 0);
     }
-	//Este es otro comentario sobre un metodo test
+	//Este es otro comentario sobre un metodo test repo3
     @Test
     public void validateBInsert() {
         studentDataAccessService.insertStudent(getUUID(), getStudent());
         List<Student> students = studentDataAccessService.selectAllStudents();
 
         assertNotNull(students.get(0).getStudentId());
-        assertEquals(students.get(0).getFirstName(), "Leano");
-        assertEquals(students.get(0).getLastName(), "Mitocode");
-        assertEquals(students.get(0).getEmail(), "Iris@mitocode.com");
+        assertEquals(students.get(0).getFirstName(), "repo3");
+        assertEquals(students.get(0).getLastName(), "repo3");
+        assertEquals(students.get(0).getEmail(), "repo3@mitocode.com");
         assertEquals(students.get(0).getGender(), Student.Gender.FEMALE);
     }
 
-	//Este codigo permite hacer un test
+	//Este codigo permite hacer un test repo3
     @Test
     public void validateCUpdateFirstName() throws UnsupportedEncodingException {
         studentDataAccessService.updateFirstName(getUUID(), "Melissa");
         List<Student> students = studentDataAccessService.selectAllStudents();
 
         assertEquals(students.get(0).getStudentId().toString(), getUUID().toString());
-        assertEquals(students.get(0).getFirstName(), "Lenin");
-        assertEquals(students.get(0).getLastName(), "Mitocode");
-        assertEquals(students.get(0).getEmail(), "Samira@mitocode.com");
+        assertEquals(students.get(0).getFirstName(), "repo3");
+        assertEquals(students.get(0).getLastName(), "repo3");
+        assertEquals(students.get(0).getEmail(), "repo3@mitocode.com");
         assertEquals(students.get(0).getGender(), Student.Gender.FEMALE);
     }
-
+	//repo3
     private Student getStudent() {
-        return new Student(getUUID(), "Pilar", "Mitocode", "Pilar@mitocode.com", Student.Gender.FEMALE);
+        return new Student(getUUID(), "repo3", "repo3", "repo3@mitocode.com", Student.Gender.FEMALE);
     }
-	
+	//repo3
     private UUID getUUID(){
         String source = "c15f3559-abdf-4270-06e2-379c1be40b6f";
         return UUID.fromString(source);
